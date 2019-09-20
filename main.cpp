2fce63a0d4276bc961a4b4dc0c0590a7ff2c7485
Lluis <16152037+LluisMas@users.noreply.github.com>
2019-09-20 20:42:36 +0200

    Params

--- main.cpp
+++ main.cpp
@@ -3,13 +3,22 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char const *argv[])
 {
-  int height = 20;
-  int width = 20;
+  if (argc < 2)
+  {
+    printf("Not enough arguments\n");
+    return 0;
+  }
+
+  if (atoi(argv[1]) < 10 || atoi(argv[2]) < 10)
+  {
+    printf("Min dimension is 10x10\n");
+    return 0;
+  }
 
   srand(time(0));
-  Map map(height, width);
+  Map map(atoi(argv[1]), atoi(argv[2]));
   cout << map;
   map.showInfo();
 
